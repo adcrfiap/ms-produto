6b829453c44e978e3bddc3d840b067db4067e907
adcr <caique.macedo@targetdata.com.br>
2021-03-21 01:59:36 -0300

    Configurando o swagger na aplicação.

--- src/main/java/br/com/fiap/arremate/msproduto/config/SwaggerConfig.java
+++ src/main/java/br/com/fiap/arremate/msproduto/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.fiap.arremate.mscomprador"))
+                .apis(RequestHandlerSelectors.basePackage("br.com.fiap.arremate.msproduto"))
                 .paths(PathSelectors.any())
                 .build();
     }
